a3d47ce96a7a3b6094a58f9a8a5cfc65cfb2af1f
Rhys <72167211+Smunk3y@users.noreply.github.com>
2023-09-14 11:30:27 -0400

    Commented JS

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Function to fetch and display team information
 async function displayTeamInfo() {
   // For each HTML div / p
   const teamNameDiv = document.getElementById('team-name');
@@ -13,7 +12,6 @@ async function displayTeamInfo() {
   const gamesPlayedDiv = document.getElementById('games-played');
   const winstreakDiv = document.getElementById('win-streak');
 
-
   const staticValueDiv = document.getElementById('static-value');
   const selectedTeamAbbreviation = staticValueDiv.textContent.trim();
 
@@ -22,17 +20,18 @@ async function displayTeamInfo() {
 
     const response = await fetch(apiUrl);
     if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
+      throw new Error(`HTTP error! Status: ${response.status}`); //error if dont connect to api
     }
 
+    //take data from the api
     const data = await response.json();
     const team = data.team;
     const nextEvent = team.nextEvent[0];
     const opponentShortName = nextEvent.shortName;
 
+    //search for the links from espn to link for more info
     const rosterLink = data.team.links.find(link => link.rel.includes('roster'));
     const statsLink = data.team.links.find(link => link.rel.includes('stats'));
-
 
 
     // Set the content of each div element
@@ -45,7 +44,7 @@ async function displayTeamInfo() {
 
     teamLogoImg.src = team.logos[0].href;
 
-    // Display Points Per Game wild code
+    // search for each extra point thing differnetly
     pointsForDiv.textContent = `Avg Points For: ${team.record.items[0].stats.find(stat => stat.name === 'avgPointsFor').value}`;
     pointsAgainstDiv.textContent = `Avg Points Against: ${team.record.items[0].stats.find(stat => stat.name === 'avgPointsAgainst').value}`;
     pointsDifDiv.textContent =  `Point Differential: ${team.record.items[0].stats.find(stat => stat.name === 'differential').value}`;
@@ -54,39 +53,39 @@ async function displayTeamInfo() {
     gamesPlayedDiv.textContent =  `Games Played: ${team.record.items[0].stats.find(stat => stat.name === 'gamesPlayed').value} / 17`;
 
 
-    //for roster link
+    //for roster & Stats ESPN linking to a element in the html for a hotlink 
     if (rosterLink) {
-    // Create an <a> element
+
     const rosterLinkElement = document.createElement('a');
 
-    // Set the href attribute and text content
+
     rosterLinkElement.href = rosterLink.href;
     rosterLinkElement.target ='_blank';  //open in new tab
-    rosterLinkElement.textContent = 'Roster'; // You can customize the link text
+    rosterLinkElement.textContent = 'Roster';
 
-    // Append the <a> element to a container div on your webpage
+
     const containerDiv = document.getElementById('roster-link-container');
     containerDiv.appendChild(rosterLinkElement);
-  }
-  //For stats link
-  if (statsLink) {
-  // Create an <a> element for statistics
-  const statsLinkElement = document.createElement('a');
-
-  // Set the href attribute and text content
-  statsLinkElement.href = statsLink.href;
-  statsLinkElement.target ='_blank'; //open in new tab
-  statsLinkElement.textContent = 'Statistics'; // You can customize the link text
-
-  // Append the <a> element to a container div on your webpage
-  const containerDiv = document.getElementById('stats-link-container');
-  containerDiv.appendChild(statsLinkElement);
-}
-
-
-    } catch (error) {
-      console.error('Error fetching data:', error);
     }
+
+    if (statsLink) {
+
+    const statsLinkElement = document.createElement('a');
+
+
+    statsLinkElement.href = statsLink.href;
+    statsLinkElement.target ='_blank'; //open in new tab
+    statsLinkElement.textContent = 'Statistics';
+
+
+    const containerDiv = document.getElementById('stats-link-container');
+    containerDiv.appendChild(statsLinkElement);
+  }
+
+
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
   }
 
 window.onload = displayTeamInfo;
